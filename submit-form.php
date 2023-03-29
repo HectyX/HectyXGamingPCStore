b9579522e42b7a00d39e000d03fe207916b40202
BlexsYT <110706607+BlexsYT@users.noreply.github.com>
2023-03-29 10:46:50 -0700

    Update submit-form.php

--- submit-form.php
+++ submit-form.php
@@ -1,5 +1,22 @@
-<html>
-  <header>
-  <li>The contact us service isn't live right now, sorry! You can contact us directly or join our discord<a href="https://discord.gg/wJBGpNfJaM">here</a></li>
-  </header>
-<html>
+<?php
+
+if(isset($_POST['submit'])) {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
+
+    $to = 'hectyxcorp@hectyx.ga';
+    $subject = 'New message from your website';
+    $message = "Name: $name\n\nEmail: $email\n\nMessage: $message";
+
+    $headers = "From: $email\r\nReply-To: $email\r\n";
+
+    if(mail($to, $subject, $message, $headers)) {
+        echo 'Your message has been sent.';
+    } else {
+        echo 'There was a problem sending your message.';
+    }
+}
+
+?>
